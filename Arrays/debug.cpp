128bee07cda91622b8d886517b902c4e935f38e9
Pdeo55 <88934417+Pdeo55@users.noreply.github.com>
2023-06-07 19:03:47 +0530

    Create debug.cpp

---
+++ Arrays/debug.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//    int arr[5] = {1, 2, 3, 4, 5};
+//    for(int i = 1; i <= 5; i++) {   // initialize with 0 to 5
+//       cout << arr[i] << " ";
+//    }  
+
+
+//2
+
+   return 0;
+}
