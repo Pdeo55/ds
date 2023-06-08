c5497be2463a7f92f1609348ba9a3a34789ffce2
Pdeo55 <88934417+Pdeo55@users.noreply.github.com>
2023-06-08 16:41:16 +0530

    Create findMissing.cpp

---
+++ Vector/findMissing.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Missing(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int index = abs(arr[i]);
+        if (arr[index - 1] > 0)
+        {
+            arr[index - 1] *= -1;
+        }
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+      if(arr[i]>0){
+        cout<<i+1<<" ";
+      }
+    }
+    
+}
+int main()
+{
+    vector<int> arr = {3, 3, 5, 3,4 };
+    Missing(arr);
+
+    return 0;
+}
